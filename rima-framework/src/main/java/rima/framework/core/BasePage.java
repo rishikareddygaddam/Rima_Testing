11b77973cdb40313f43776165fc10e79733b7aaf
rishikareddygaddam <rishika.reddy.g5@gmail.com>
2020-05-28 15:55:04 -0400

    initial code changes to BasePage.java

--- rima-framework/src/main/java/rima/framework/core/BasePage.java
+++ rima-framework/src/main/java/rima/framework/core/BasePage.java
@@ -38,6 +38,6 @@ public class BasePage {
     }
 
     protected void waitForTextToDisappear(By locator, String text) {
-        wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(locator, text1)));
+        wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(locator, text)));
     }
 }
