5b08acb5dc6be8519fcbc0d52f6c320b15c75d3d
rishikareddygaddam <rishika.reddy.g5@gmail.com>
2020-05-28 15:50:13 -0400

    initial code changes to BasePage.java

---
+++ rima-framework/src/main/java/rima/framework/core/BasePage.java
@@ -0,0 +1,43 @@
+package rima.framework.core;
+// In the framework module, we create a class called BasePage.java.
+// All page object classes will extend the BasePage, thus inheriting all the base methods.
+// Our BasePage class will have a constructor which takes a WebDriver object to initialize
+// a WebDriverWait object.
+// The constructor will also be responsible to initialize WebElements via PageFactory.
+// In addition, we will also have some utility wait methods to handle the various waits
+// such as WaitForElementToAppear.
+// So, the BasePage class will look like:
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    private static final int TIMEOUT = 5;
+    private static final int POLLING = 100;
+
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, TIMEOUT, POLLING);
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
+    }
+
+    protected void waitForElementToAppear(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected void waitForElementToDisappear(By locator) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    protected void waitForTextToDisappear(By locator, String text) {
+        wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(locator, text)));
+    }
+}
